81b6add2089bc27b2bd2b76caca755a481bb73e3
lapagliadm <lapagliadm@s.dcsdk12.org>
2021-02-11 11:09:30 -0700

    Added all pages laoshifengkuistasiack

---
+++ app/src/main/java/org/legendtitans/mypaperstory/PageThree.java
@@ -0,0 +1,14 @@
+package org.legendtitans.mypaperstory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_three);
+    }
+}
