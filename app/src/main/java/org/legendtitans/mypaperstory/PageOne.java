e3a388ce0eb4681db91a72b7d809ac0117e34e4a
lapagliadm <lapagliadm@s.dcsdk12.org>
2021-02-11 11:03:54 -0700

    Added Page One

---
+++ app/src/main/java/org/legendtitans/mypaperstory/PageOne.java
@@ -0,0 +1,14 @@
+package org.legendtitans.mypaperstory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_one);
+    }
+}
